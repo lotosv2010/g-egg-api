d176ca5116aad0e7ff179e22c6fd2d97b5758eec
gaowenbin <lotosv2010@163.com>
2022-07-12 16:58:27 +0800

    feat(v1.0): 用户登录

--- app/controller/user.js
+++ app/controller/user.js
@@ -12,7 +12,7 @@ class UserController extends Controller {
       username: { type: 'string' },
       email: { type: 'email' },
       password: { type: 'string' },
-    });
+    }, body);
 
     // 检验用户是否存在
     const { username } = body;
@@ -27,6 +27,46 @@ class UserController extends Controller {
 
     // ! 2.保存用户
     const user = await userService.createUser(body);
+
+    // ! 3.生成 token
+    const token = await userService.createToken({
+      userId: user._id,
+    });
+    // ! 4.发送响应
+
+    ctx.body = {
+      user: {
+        email: user.email,
+        username: user.username,
+        token,
+        avatar: user.avatar,
+        channelDescription: user.channelDescription,
+      },
+    };
+  }
+  async login() {
+    const { ctx, service: { user: userService } } = this;
+    const { body } = ctx.request;
+    const { md5 } = ctx.helper;
+    const { email, password } = body;
+
+    // ! 1.数据校验
+    ctx.validate({
+      email: { type: 'email' },
+      password: { type: 'string' },
+    }, body);
+
+    // ! 2.获取用户
+    // 检验用户是否存在
+    const user = await userService.findByEmail(email);
+    if (!user) {
+      ctx.throw(422, '用户不已存在');
+    }
+
+    // 检验密码是否正确
+    if (user.password !== md5(password)) {
+      ctx.throw(422, '密码不正确');
+    }
 
     // ! 3.生成 token
     const token = await userService.createToken({
--- app/service/user.js
+++ app/service/user.js
@@ -4,6 +4,15 @@ class UserService extends Service {
   get User() {
     return this.app.model.User;
   }
+  get md5() {
+    return this.ctx.helper.md5;
+  }
+  get jwt() {
+    return this.app.config.jwt;
+  }
+  get sign() {
+    return this.ctx.helper.sign;
+  }
   async findByUsername(username) {
     return await this.User.findOne({
       username,
@@ -12,15 +21,15 @@ class UserService extends Service {
   async findByEmail(email) {
     return await this.User.findOne({
       email,
-    });
+    }).select('+password');
   }
   async createUser(user) {
-    user.password = this.ctx.helper.md5(user.password);
+    user.password = this.md5(user.password);
     return await this.User.create(user);
   }
   async createToken(data) {
-    const { secret, expiresIn } = this.app.config.jwt;
-    return await this.ctx.helper.sign(data, secret, { expiresIn });
+    const { secret, expiresIn } = this.jwt;
+    return await this.sign(data, secret, { expiresIn });
   }
 }
 
